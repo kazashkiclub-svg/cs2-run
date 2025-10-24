fec85c7cd64b28fb906afb04ed9691ba20f582a3
kazashkiclub-svg <kazashkiclub@gmail.com>
2025-10-25 02:04:26 +0500

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // включаем статическую сборку
-  images: {
-    unoptimized: true, // отключаем оптимизацию картинок (иначе export упадет)
-  },
-};
-
-export default nextConfig;
